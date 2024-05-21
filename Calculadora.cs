f6e0457931c137d46aa13744c656f909d6bb812d
matisagra <sagramatias04@gmail.com>
2024-05-21 16:29:32 -0300

    punto 2 a medias

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace EspacioCalculadora;
 
@@ -39,4 +40,47 @@ public class Calculadora
 
 
 
+}
+
+//Punto 2
+namespace Empleado;
+
+public class Empleado
+{
+    string Nombre;
+    string Apellido;
+    DateTime FechaNac;
+    char EstadoCivil;
+    DateTime Ingreso;
+    double Sueldo;
+    enum cargos
+    {
+        Auxiliar,
+        Administrativo,
+        Ingeniero,
+        Especialista,
+        Investigador
+    }
+    cargos cargo;
+
+    DateTime hoy = DateTime.Now;
+
+
+    
+
+    public void Antiguedad()
+    {
+        int anti = hoy.year - Ingreso.year;
+    }
+
+    public void Edad()
+    {
+        int edad = hoy.year - FechaNac.year;
+
+    }
+
+    public int Jubilacion()
+    {
+        int jubi = 65 - edad;
+    }
 }
