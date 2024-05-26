b865d8c6cda6d633f72e296568ae7a7b7f6815bd
Matias <sagramtias04@gmail.com>
2024-05-26 18:16:58 -0300

    punto 2 terminado

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,86 +1,114 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-
-namespace EspacioCalculadora;
-
-public class Calculadora
+namespace EspacioCalculadora
 {
-
-    public double dato;
-
-    public void Sumar(double termino)
+    public class Calculadora
     {
-        dato += termino;
+        public double dato;
 
+        public void Sumar(double termino)
+        {
+            dato += termino;
+        }
+
+        public void Restar(double termino)
+        {
+            dato -= termino;
+        }
+
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
+        }
+
+        public void Dividir(double termino)
+        {
+            dato /= termino;
+        }
+
+        public void Limpiar()
+        {
+            dato = 0;
+        }
     }
 
-    public void Restar(double termino)
+    public class Empleado
     {
-        dato -= termino;
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNac;
+        private char estadoCivil;
+        private DateTime ingreso;
+        private double sueldo;
+        private Cargos cargo;
+
+        public enum Cargos
+        {
+            Auxiliar,
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
+        }
+
+        public Empleado(string nombre, string apellido, DateTime fechaNac, char estadoCivil, DateTime ingreso, double sueldo, Cargos cargo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNac = fechaNac;
+            this.estadoCivil = estadoCivil;
+            this.ingreso = ingreso;
+            this.sueldo = sueldo;
+            this.cargo = cargo;
+        }
+
+        public int Antiguedad()
+        {
+            DateTime hoy = DateTime.Now;
+            return hoy.Year - ingreso.Year;
+        }
+
+        public int Edad()
+        {
+            DateTime hoy = DateTime.Now;
+            return hoy.Year - fechaNac.Year;
+        }
+
+        public int Jubilacion()
+        {
+            int edad = Edad();
+            return 65 - edad;
+        }
+
+         public double CalcularSalario()
+        {
+            double adicional = 0;
+            int antiguedad = Antiguedad();
+
+            // Calcular adicional por antigüedad
+            if (antiguedad <= 20)
+            {
+                adicional = sueldo * 0.01 * antiguedad;
+            }
+            else
+            {
+                adicional = sueldo * 0.25;
+            }
+
+            // Incrementar adicional si el cargo es Ingeniero o Especialista
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional *= 1.5;
+            }
+
+            // Incrementar adicional si es casado
+            if (estadoCivil == 'C')
+            {
+                adicional += 150000;
+            }
+
+            return sueldo + adicional;
+        }
 
     }
-
-    public void Multiplicar(double termino)
-    {
-        dato *= termino;
-
-    }
-
-    public void Dividir(double termino)
-    {
-        dato = dato / termino;
-
-    }
-
-    public void Limpiar()
-    {
-        dato = 0;
-    }
-
-
-
 }
 
-//Punto 2
-namespace Empleado;
 
-public class Empleado
-{
-    string Nombre;
-    string Apellido;
-    DateTime FechaNac;
-    char EstadoCivil;
-    DateTime Ingreso;
-    double Sueldo;
-    enum cargos
-    {
-        Auxiliar,
-        Administrativo,
-        Ingeniero,
-        Especialista,
-        Investigador
-    }
-    cargos cargo;
-
-    DateTime hoy = DateTime.Now;
-
-
-    
-
-    public void Antiguedad()
-    {
-        int anti = hoy.year - Ingreso.year;
-    }
-
-    public void Edad()
-    {
-        int edad = hoy.year - FechaNac.year;
-
-    }
-
-    public int Jubilacion()
-    {
-        int jubi = 65 - edad;
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using EspacioCalculadora;
 
-Calculadora calcu = new Calculadora();
+/* Calculadora calcu = new Calculadora();
 
 string opcion;
 do
@@ -55,8 +55,31 @@ do
     Console.WriteLine($"El resultado es: {resultado}");
 
 
-} while (opcion != "0");
+} while (opcion != "0"); */
 
 
+// Punto 2
+
+        Empleado[] empleados = new Empleado[3];
+
+        empleados[0] = new Empleado("Juan", "Pérez", new DateTime(1980, 5, 23), 'C', new DateTime(2010, 3, 15), 500000, Empleado.Cargos.Ingeniero);
+        empleados[1] = new Empleado("Ana", "García", new DateTime(1975, 7, 30), 'S', new DateTime(2005, 6, 20), 650000, Empleado.Cargos.Administrativo);
+        empleados[2] = new Empleado("Luis", "Martínez", new DateTime(1990, 11, 10), 'C', new DateTime(2015, 1, 10), 400000, Empleado.Cargos.Especialista);
+
+        double montoTotalSalarios = 0;
+        Empleado empleadoProximoJubilacion = empleados[0];
+
+        foreach (Empleado empleado in empleados)
+        {
+            montoTotalSalarios += empleado.CalcularSalario();
+
+            if (empleado.Jubilacion() < empleadoProximoJubilacion.Jubilacion())
+            {
+                empleadoProximoJubilacion = empleado;
+            }
+        }
+
+        Console.WriteLine($"Monto Total de Salarios: {montoTotalSalarios:C}");
+        Console.WriteLine($"Empleado más próximo a jubilarse: {empleadoProximoJubilacion}");
 
 
